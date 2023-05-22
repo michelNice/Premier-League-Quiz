6438ac15cd6ed17b492354fecb0054817014221f
michel <michel-gouveia1997@hotmail.com>
2023-05-22 11:09:30 -0700

    New Project

---
+++ js/main.js
@@ -0,0 +1,54 @@
+//Quiz Questions
+const quizData = [
+    {
+        question:'who has scored most goals in one premier league season',
+        a:'Erling Haaland',
+        b:'Cristiano Ronaldo',
+        c:'Kevin Phillis',
+        d:'Luiz Suares'
+    },
+
+    {
+        question:' Which team  won three Premier league titles in the row in the last 3 years?',
+        a:'Man Unided',
+        b:'Liverpol',
+        c:'Chelsea',
+        d:'Arsenal',
+    }
+    ,
+    {
+        question:'Who are the most valuable players in the Premier League?',
+        a:'Gabriel Jesus',
+        b:'hary Kane',
+        c:'kevin de bruyne',
+        d:'Erling Haaland',
+    
+    },
+
+    {
+        question:'When last did Newcastle play Champions League?',
+        a:'2001-02',
+        b:'2002-03',
+        c:'2004-05',
+        d:'2010-12',
+    },
+
+   
+    {
+        question:'Which team won Premier League title 2022-23',
+        a: 'West Ham',
+        b:'Arsenal',
+        c:'Man Unided',
+        d:'Wolves',
+    }
+]
+
+
+const test = function(test){
+    console.log(test)
+}
+
+
+test(quizData)
+
+
